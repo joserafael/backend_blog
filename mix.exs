976d745165eca10f1eeec964bcdc4dc577a4f1f1
Jose Rafael Camejo <jose20camejo@gmail.com>
2025-07-17 17:18:17 -0300

    install scrivener_ecto to paginate posts

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule BackendBlog.MixProject do
       {:guardian, "~> 2.0"},
       {:dotenv, "~> 3.0.0", only: [:dev, :test]},
       {:faker, "~> 0.17", only: :dev},
+      {:scrivener_ecto, "~> 2.7"},
     ]
   end
 
